bc059a0268b7725418dbde3de46462f6c3077e97
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-24 14:55:35 +0200

    Solution aquapark

--- app/main.py
+++ app/main.py
@@ -2,24 +2,60 @@ from abc import ABC
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int):
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __get__(self, obj, owner) -> int:
+        return getattr(obj, self.protected_name)
+
+    def __set__(self, obj, value: int):
+        if not isinstance(value, int):
+            raise TypeError()
+        elif not (self.min_amount <= value <= self.max_amount):
+            raise ValueError()
+        setattr(obj, self.protected_name, value)
+
+    def __set_name__(self, owner, name: str) -> None:
+        self.protected_name = "_" + name
 
 
 class Visitor:
-   pass
+   def __init__(self, name: str,age: int, weight: int, height: int):
+       self.name = name
+       self.age = age
+       self.weight = weight
+       self.height = height
 
 
 class SlideLimitationValidator(ABC):
-    pass
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str, limitation_class: type):
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        try:
+            self.limitation_class(visitor.age,
+                                  visitor.weight, visitor.height)
+            return True
+        except (ValueError, TypeError):
+            return False
